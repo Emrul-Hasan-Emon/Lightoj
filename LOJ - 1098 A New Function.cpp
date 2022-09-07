375c1a6103d8749d0a7c03d694a0a1231686dcbb
Emrul-Hasan-Emon <74479414+Emrul-Hasan-Emon@users.noreply.github.com>
2022-09-07 21:54:33 +0600

    Create LOJ - 1098 A New Function.cpp

---
+++ LOJ - 1098 A New Function.cpp
@@ -0,0 +1,105 @@
+Problem Link: https://lightoj.com/problem/a-new-function
+For Better Understanding: http://rizoantoufiq.blogspot.com/2015/12/lightoj-1098-new-function.html
+
+///              B I S M I L L A H I R  R A H M A N I R  R A H I M
+
+// ============================================================================ //
+// ||                                                                        || //
+// ||             International University of Business Agriculture           || //
+// ||                    and Technology, Dhaka, Bangladesh                   || //
+// ||                           Emrul Hasan Emon                             || //
+// ||                                                                        || //
+// ============================================================================ //
+
+#include<bits/stdc++.h>
+#include<vector>
+#include<set>
+#include<map>
+#include<queue>
+using namespace std;
+typedef bool                      boo;
+typedef int                       li;
+typedef long long int             lu;
+typedef unsigned long long int    ll;
+typedef double                    db;
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+typedef vector < li >                   vli;
+typedef vector < ll >                   vll;
+typedef set < li >                      sli;
+typedef set < ll >                      sll;
+typedef pair < li, li >                pli;
+typedef pair < ll, li >                pll;
+typedef map < li,li >                  mli;
+typedef map < ll,ll >                  mll;
+typedef vector < pair < li, li > >     vpi;
+typedef vector < pair < ll, ll > >     vpl;
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#define tc                        int t;cin>>t;while(t--)
+#define inp_i(a,n)                for(int ii = 0; ii < n ; ii++) cin >> a[ii]
+#define out_i(a, b, c)            for(int ii = b; ii < c; ii++) cout << a[ii] spc; cout nl;
+#define lp(i,a,b)                 for(i=a;i<b;i++)
+#define len(z)                    z.begin(),z.end()
+#define faster                    ios::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+#define input_txt                 freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define nl                        <<"\n"
+#define spc                       <<" "
+#define sp                        <<" "<<
+#define co(x)                     cout<<x nl
+#define sz(a)                     a.size()
+#define cy                        cout<<"YES" nl
+#define cn                        cout<<"NO" nl
+#define pb                        push_back
+#define F                         first
+#define S                         second
+#define pi                        2*acos(0.0)
+#define clr(z)                    z.clear()
+#define rn                        return
+#define gcd(a,b)                  __gcd(a,b)
+#define mem(b,z)                  memset(b,z,sizeof(b))
+#define fixed(x)                  fixed<<setprecision(x)
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const ll lx = 2e6 + 7;
+const ll mod = 998244353;
+
+void solve()
+{
+    int t;
+    cin >> t;
+    for(int cs = 1; cs <= t; cs++)
+    {
+    	ll n, i, j;
+    	cin >> n;
+    	ll ans = 0;
+    	
+    	for(i = 2; i*i <= n; i++)
+    	{
+    		j = n/i;
+    		ans += (j+i) * (j-i+1) / 2;
+    		ans += i*(j-i);
+    	}
+    	cout << "Case " << cs << ": " << ans << '\n';
+    }
+}
+
+void init_code()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+
+int main()
+{
+    faster
+    
+    solve();
+
+// A L H A M D U L I L L A H
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 2 3 4 5....
+// Emrul Hasan Emon
+}
